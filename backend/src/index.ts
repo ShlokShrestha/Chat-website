1a82b128b815e1524c01100e5afeb009140e4ce1
shlokshrestha <shlokstha99@gmail.com>
2025-02-10 15:30:32 +0545

    added socket in backend and frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,15 +4,15 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import cors from "cors";
+import { app, server } from "./socket/socket";
 dotenv.config();
 
-const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(8080, () => {
+server.listen(8080, () => {
   console.log("Port running in 8080");
 });
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import SocketContextProvider from "./context/SocketContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthContextProvider>
-      <App />
+      <SocketContextProvider>
+        <App />
+      </SocketContextProvider>
     </AuthContextProvider>
   </BrowserRouter>
 );
